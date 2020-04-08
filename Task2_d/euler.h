ef9a5a2e843e27b6f6d17c11253300f3ef0f7ac5
EricAntosch <ericantosch@web.de>
2020-04-08 19:25:11 +0200

    Completed all of the main tasks

---
+++ Task2_d/euler.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef EULER_H_INCLUDED
+#define EULER_H_INCLUDED
+
+double diff(double euler, double result);
+double eulNum(double euler, int* i);
+
+#endif
+
--- Task2_d/main.c
+++ Task2_d/main.c
@@ -1,44 +1,52 @@
 #include<stdio.h>
 #include<math.h>
+#include "euler.h"
 
 
 
-double eulNum(double euler) {
+
+double eulNum(double euler, int* i) {
 	double result = 1;
 	double fac = 1;
 	double euler_x = 1;
 	double resultEuler = 1;
-	for (int i = 1; i < 10; i++)
-	{	
-			fac *= i;
-			euler_x *= euler;
-			resultEuler = euler_x/fac;
-		
+	do
+	{
+		fac *= *i;
+		euler_x *= euler;
+		resultEuler = euler_x / fac;
+
+
+		if (isnan(resultEuler) != 0) {
+			break;
+		}
+		*i = *i + 1;
 		result += resultEuler;
-	}
+	} while (diff(euler, result) > 0);
 	return result;
 }
-double diff(double euler) {
-
-	double difference = exp(euler) - eulNum(euler);
-	return (difference / exp(euler)) * 100;
+double diff(double euler, double result) {
+	return exp(euler) - result;
 }
 
 int main(void) {
 
 	double euler;
+	int i = 1;
 	do {
 		printf("Geben Sie bitte eine Zahl ein, von der der Expotentialwert bestimmt werden soll!\n");
 		scanf_s("%lf", &euler);
 		if (euler != 0) {
-			printf("%lf\n", eulNum(euler));
-			printf("Der Unterschied zwischen der richtigen und der errechneten Zahl betr%cgt %lf%%\n", 132, diff(euler));
+			printf("%lf\n", eulNum(euler, &i));
+			printf("Die Anzahl der Summanden betr%cgt %d\n", 132, i);
 			printf("Um das Programm zu beenden, geben Sie bitte eine %d ein.\n", 0);
+			i = 1;
 		}
 		else
 		{
 			break;
 		}
+		
 	} while (1 == 1);
 	return 0;
 }
